b288e4df3fe7695e5eaa57d0942f665e6964b126
Sipeng He <hsp8412@hotmail.com>
2022-11-13 22:51:05 -0700

    Add email me feature

--- src/components/contactInfo.jsx
+++ src/components/contactInfo.jsx
@@ -1,51 +1,125 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../css/contact.css";
-import { Container } from "react-bootstrap";
+import { Container, FloatingLabel, Form } from "react-bootstrap";
+import { solid, regular } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import emailjs from "@emailjs/browser";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
 
 const ContactInfo = () => {
+  const serviceId = process.env.REACT_APP_SERVICE_ID;
+  const templateId = process.env.REACT_APP_TEMPLATE_ID;
+  const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+
+  const {
+    handleChange,
+    handleBlur,
+    errors,
+    values,
+    touched,
+    handleSubmit,
+    setFieldValue,
+  } = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      message: "",
+    },
+    onSubmit: async ({ username, email, message }, { resetForm }) => {
+      emailjs
+        .send(
+          serviceId,
+          templateId,
+          {
+            to_name: "Sipeng He",
+            from_name: username,
+            message,
+            reply_to: email,
+          },
+          publicKey
+        )
+        .then(
+          function (response) {
+            toast.success("Thank you for your message!");
+            resetForm();
+          },
+          function (error) {
+            toast.error(error.message);
+          }
+        );
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .max(50, "Username must be less than 50 characters")
+        .required("Username is required"),
+      email: Yup.string()
+        .email("Please enter a valid email address")
+        .max(128)
+        .required("Email is required"),
+      message: Yup.string().max(2000).required("Message is required"),
+    }),
+  });
+
   return (
-    <div className="card mb-4">
-      <div className="card-body about-card">
-        <h2 className="about-title">Contact-info</h2>
+    <div className="card mb-4 p-3">
+      <div className="card-body text-start ">
+        <h2 className="about-title">
+          <FontAwesomeIcon icon={solid("envelope")} className="me-3" />
+          Send me a message
+        </h2>
+
         <div className="about-text">
-          If you have any questions about me, please email me at{" "}
-          <a
-            href="mailto:sipeng.he@ucalgary.ca"
-            target="_blank"
-            rel="noopener"
-            className="contact-email"
-          >
-            sipeng.he@ucalgary.ca
-          </a>
-          .
-          <div>
-            You may also find me on{" "}
-            <a
-              href="https://www.linkedin.com/in/sipeng-he-250654190/"
-              target="_blank"
-              className="contact-link"
-            >
-              Linkedin
-            </a>{" "}
-            or{" "}
-            <a
-              href="https://www.instagram.com/hsp_steven/"
-              target="_blank"
-              className="contact-link"
-            >
-              Instagram
-            </a>
-            .
-          </div>
-          <p>I will reply to your message ASAP.</p>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=""
+                name="username"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.username}
+              />
+              <Form.Text className="text-danger">
+                {touched.username && errors.username ? errors.username : null}
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder=""
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              <Form.Text className="text-danger">
+                {touched.email && errors.email ? errors.email : null}
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                placeholder=""
+                className="message-textarea"
+                name="message"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.message}
+              />
+              <Form.Text className="text-danger">
+                {touched.message && errors.message ? errors.message : null}
+              </Form.Text>
+            </Form.Group>
+            <button className="send-btn mt-3" type="submit">
+              Send
+            </button>
+          </Form>
         </div>
-        <Container className="d-flex justify-content-center">
-          <img
-            src="/contact-img.jpeg"
-            alt="contact-img"
-            className="contact-img"
-          />
-        </Container>
       </div>
     </div>
   );
